7b7fa2fd8f2970d20272b0d90352d743887b3e92
Matt <mattfsewell@gmail.com>
2021-03-16 18:12:26 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,38 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      // username: '', 
+      // password: ''
+    };
   }
 
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+  handleSubmit = e => {
+    e.preventDefault()
+    if (this.state.username && this.state.password){
+      this.props.handleLogin(this.state)
+    }
+    else {return}
+    // debugger
+  }
   render() {
     return (
-      <form>
+      <form onSubmit={(e) => this.handleSubmit(e)}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input onChange={e => this.handleChange(e)} value={this.state.username} id="username" name="username" type="text" />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input onChange={e => this.handleChange(e)} value={this.state.password} id="password" name="password" type="password" />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,23 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ''
+    };
+  }
+  handleChange = e => {
+    // debugger
+    this.setState({
+      message: e.target.value
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input onChange={e => this.handleChange(e)} value={this.state.message} type="text" name="message" id="message" />
+        <div>Remaining Characters: {this.props.maxChars - this.state.message.length}</div>
       </div>
     );
   }
